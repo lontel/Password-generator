40ea1fe6edc5479d373dc05ff07c5be46d9a08d1
lontel <lonte.leon@gmail.com>
2023-03-07 12:59:45 +0100

    my first Angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pass-gen';
+  
+  password = '';
+  includesLetters = false;
+  includesNumbers = false;
+  includesSymbols = false;
+  passLength = 0;
+
+  onChangeLetters() {
+    this.includesLetters = !this.includesLetters
+  }
+
+  onChangeNumber() {
+    this.includesNumbers = !this.includesNumbers
+  }
+
+  onChangeSymbol() {
+    this.includesSymbols = !this.includesSymbols
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value)
+    if (!isNaN(parsedValue)){
+      this.passLength = parsedValue
+    }
+  }
+
+    handleButton() {
+
+    let validChars = ''
+
+    const validLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYXZ'
+    const validNumbers = '0123456789'
+    const validSymbols = '±!@#$%^&*()'
+
+    if(this.includesLetters) {
+      validChars += validLetters
+    }
+
+    if(this.includesNumbers) {
+      validChars += validNumbers
+    }
+
+    if(this.includesSymbols) {
+      validChars += validSymbols
+    }
+
+    let generatedPass = ''
+    for (let i = 0; i < this.passLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length)
+      generatedPass += validChars[index]
+    }
+
+    this.password = generatedPass
+  }
 }
